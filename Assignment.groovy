6d07432b3f5615c55aba84572d7860ba25875cd5
Yogendrasingh Rathore <yogendrasingh.rathor@cue028>
2019-11-08 18:12:26 +0530

    Assignment Updated

--- Assignment.groovy
+++ Assignment.groovy
@@ -1,15 +1,16 @@
 import java.io.File
+import groovy.json.JsonOutput
 
 class Validate
 {
     static void main(String[] arg)
     {
         def f = new File("Value.csv")
-        def f1 = new File("/home/cuelogic.local/yogendrasingh.rathor/Notes/Groovy/Output.csv")
-
+        
         def data = f.text
         String[] line = data.split("\n")
-        
+       
+        f.text = ''
 
         for (i in 0..<line.size())
         {
@@ -20,33 +21,69 @@ class Validate
             String a = elements[1] 
             String u = elements[2] 
     
-            def regex_email = /\w+@\w+\.\w{2,6}/
-            if (!(e ==~ regex_email))
+            f.append("${line[i]}")
+
+            def regex_email = /\w+@\w+\.\w{3}/
+            def regex_email2 = /\w+@\w+\.\w{2}\.\w{2}/
+            def regex_email3 = /\w+@\w+\.\w{3}\.\w{2}/
+
+            if (!(e ==~ regex_email) && !(e ==~ regex_email2) && !(e ==~ regex_email3))
             {
-                f1.append('\n Invalid Email,')
+                
+                f.append(',Invalid Email,')
+            }
+            else
+            {
+                f.append(',Valid Email,')
             }
 
 
-            def regex_alpaNumeric = /\w{3}\d{3}\w{2}/
+            def regex_alpaNumeric = /[a-zA-Z]{3}[0-9]{3}[a-zA-Z]{2}/
             if (!(a ==~ regex_alpaNumeric))
             {
-                f1.append('Invalid AlphaNumeric,')
+                f.append('Invalid AlphaNumeric,')
+            }
+            else
+            {
+                f.append('Valid AlphaNumeric,')
             }
 
 
             def regex_url = $/https://www.\w+.\w{2,3}/$
             if (!(u ==~ regex_url))
             {
-                f1.append('Invalid URL')
+                f.append('Invalid URL\n')
             }
+            else
+            {
+                f.append('Valid URL\n')
+            }
+
         }
+
             
-        f1.eachLine{ line1, lineNo ->
-            println "${lineNo} ${line1}"
-         }
+            def data2 = f.text
+            String[] line2 = data2.split("\n")
+            def jsonlist = [:]
+
+            for (i in 0..<line2.size())
+            {
+                String[] elements2 = line2[i].split(",")
+
+                
+                jsonlist.put("${++i}",["${elements2[3]}","${elements2[4]}","${elements2[5]}"])
+
+
+            }
+
+           jsonlist.each { item -> println "$item.key $item.value" }
+         
+            def json = JsonOutput.toJson(jsonlist)
+            println json
 
 
     }
-     
+
+    
        
 }
